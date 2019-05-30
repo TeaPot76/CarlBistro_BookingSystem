0878b974b60e133d159c0e0816f5015579a4d7ae
lidek <lidia.stopinska@gmail.com>
2019-05-30 20:29:52 +0100

    created empty spring project, updated application.properties and added db name carl_bistro, updated pom.xml wirh dependencies

---
+++ CarlBistro/src/main/java/com/codeclan/carlbistro/CarlBistro/CarlBistroApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.carlbistro.CarlBistro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarlBistroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarlBistroApplication.class, args);
+	}
+
+}
---
+++ CarlBistro/src/test/java/com/codeclan/carlbistro/CarlBistro/CarlBistroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codeclan.carlbistro.CarlBistro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarlBistroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
