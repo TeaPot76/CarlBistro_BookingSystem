f6d93bb27b5d03a6651e582c4ac024104a29cf60
Alasdair321 <a.g.carstairs@outlook.com>
2019-06-01 13:31:23 +0100

    create Booking class

--- server/src/main/java/com/codeclan/karlbistro/models/Booking.java
+++ server/src/main/java/com/codeclan/karlbistro/models/Booking.java
@@ -1,4 +1,143 @@
 package com.codeclan.karlbistro.models;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="bookings")
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate date;
+
+    @Column(name = "time")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+    private LocalTime time;
+
+    @Column(name = "party_size")
+    private int partySize;
+
+    @ManyToOne
+    @JoinColumn(name = "booker_id", nullable = false)
+    private Booker booker;
+
+    @ManyToOne
+    @JoinColumn(name = "seatingtable_id", nullable = false)
+    private SeatingTable table;
+
+    @OneToMany(mappedBy = "booking")
+    private List<Order> orders;
+
+    @Column(name = "booking_note")
+    private String bookingNote;
+
+    public Booking(LocalDate date, LocalTime time, int partySize, Booker booker, String bookingNote) {
+        this.date = date;
+        this.time = time;
+        this.partySize = partySize;
+        this.booker = booker;
+        this.table = null;
+        this.orders = new ArrayList<>();
+        this.bookingNote = bookingNote;
+    }
+
+    public Booking() {
+    }
+
+
+
+    public void addOrder(Order order){
+        orders.add(order);
+    }
+
+    public void removeOrder(Order order){
+        orders.remove(order);
+    }
+
+    public int countOrders(){
+        return orders.size();
+    }
+
+    public double countOrdersCost(){
+        double total = 0;
+        for (Order order: orders) {
+            total += order.getMenuItem().getCost();
+        }
+        return total;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public int getPartySize() {
+        return partySize;
+    }
+
+    public void setPartySize(int partySize) {
+        this.partySize = partySize;
+    }
+
+    public Booker getBooker() {
+        return booker;
+    }
+
+    public void setBooker(Booker booker) {
+        this.booker = booker;
+    }
+
+    public SeatingTable getTable() {
+        return table;
+    }
+
+    public void setTable(SeatingTable table) {
+        if(table.getCapacity()>=partySize) {
+            this.table = table;
+        }
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public String getBookingNote() {
+        return bookingNote;
+    }
+
+    public void setBookingNote(String bookingNote) {
+        this.bookingNote = bookingNote;
+    }
 }
