c43abab1a36822df610d5b9afaa535640a2e277e
Alasdair321 <a.g.carstairs@outlook.com>
2019-06-01 10:57:46 +0100

    create booker class

--- server/src/main/java/com/codeclan/karlbistro/models/Booker.java
+++ server/src/main/java/com/codeclan/karlbistro/models/Booker.java
@@ -1,4 +1,79 @@
 package com.codeclan.karlbistro.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="bookers")
 public class Booker {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @JsonIgnoreProperties("booker")
+    @OneToMany(mappedBy = "booker")
+    private List<Booking> bookings;
+
+    public Booker(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+        this.bookings = new ArrayList<>();
+    }
+
+    public Booker() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+
+    public void addBooking(Booking booking){
+        this.bookings.add(booking);
+    }
+
+    public void removeBooking(Booking booking){
+        this.bookings.remove(booking);
+    }
+
+    public int countBookings(){
+       return bookings.size();
+    }
 }
