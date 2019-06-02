1d0d8f2fac5d8d45d1408f7ea0302be5135a323d
lidek <lidia.stopinska@gmail.com>
2019-06-02 20:42:50 +0100

    added findAll method to bookings and annotation for CORS

--- server/src/main/java/com/codeclan/karlbistro/controllers/BookingController.java
+++ server/src/main/java/com/codeclan/karlbistro/controllers/BookingController.java
@@ -1,14 +1,23 @@
 package com.codeclan.karlbistro.controllers;
 
+import com.codeclan.karlbistro.models.Booking;
 import com.codeclan.karlbistro.repositories.BookerRepository.BookerRepository;
 import com.codeclan.karlbistro.repositories.BookingRepository.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping(value = "/bookings")
 public class BookingController {
     @Autowired
     BookingRepository bookingRepository;
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @RequestMapping(value = "/bookings")
+    public List<Booking> bookings() {
+        return bookingRepository.findAll();
+    }
 }
