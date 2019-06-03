0b75b502a7878fe47ef67bc69c052d8867f18b7d
Alasdair321 <a.g.carstairs@outlook.com>
2019-06-03 06:38:13 +0100

    CrossOrigin allowed for localhost:3000 on all controllers

--- server/src/main/java/com/codeclan/karlbistro/controllers/MenuItemController.java
+++ server/src/main/java/com/codeclan/karlbistro/controllers/MenuItemController.java
@@ -3,10 +3,12 @@ package com.codeclan.karlbistro.controllers;
 import com.codeclan.karlbistro.models.MenuItem;
 import com.codeclan.karlbistro.repositories.MenuItemRepository.MenuItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping(value = "/menuItems")
 public class MenuItemController {
--- server/src/main/java/com/codeclan/karlbistro/controllers/SeatingTableController.java
+++ server/src/main/java/com/codeclan/karlbistro/controllers/SeatingTableController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping(value = "/seatingTables", method= RequestMethod.GET)
 public class SeatingTableController {
