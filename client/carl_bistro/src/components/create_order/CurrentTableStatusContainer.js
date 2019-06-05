7dc2feb45ff10965805ca6847457fe6cb4078e57
Alasdair321 <a.g.carstairs@outlook.com>
2019-06-05 11:39:39 +0100

    stateless function AddOrder added

--- client/carl_bistro/src/components/create_order/CurrentTableStatusContainer.js
+++ client/carl_bistro/src/components/create_order/CurrentTableStatusContainer.js
@@ -1,14 +1,51 @@
 import React, {
     Component
   } from "react";
+import CurrentTableTableList from "./CurrentTableTableList";
 
   class CurrentTableStatus extends Component {
       constructor(props){
           super(props)
           this.state = {
-
+            orderClicked: false
           }
       }
+
+      handleSubmit = event => {
+          return null;
+      }
+    
+
+      render(){
+          return(
+              <React.Fragment>
+                  <CurrentTableTableList
+                  
+                  />
+                  <AddOrder
+                    orderClicked={this.state.orderClicked}
+                    onSubmit={this.handleSubmit}
+                  />
+              </React.Fragment>
+          )
+      }
   }
+
+
+  function AddOrder(props){
+    if (props.orderClicked === false) {
+        return null
+    }
+    return(
+        <div>
+            <h1>Hello, I am menu item order form</h1>
+                <form onSubmit={props.onSubmit}>
+                    
+                </form>
+        </div>
+    )
+  }
+
+
 
   export default CurrentTableStatus;
