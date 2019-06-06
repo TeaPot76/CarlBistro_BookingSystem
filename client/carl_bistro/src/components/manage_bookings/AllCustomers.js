d988514550837760bb9aae334c3b1950d4fcf90c
RoryMacGregor88 <RoryMacGregor88@outlook.com>
2019-06-06 10:28:49 +0100

    Received fixed code.

--- client/carl_bistro/src/components/manage_bookings/AllCustomers.js
+++ client/carl_bistro/src/components/manage_bookings/AllCustomers.js
@@ -21,178 +21,145 @@ class AllCustomers extends Component {
     fetch(url)
       .then(res => res.json())
       .then((allBookings) => {
-          this.setState({
-              bookings: allBookings}
-            );
-          })
+        this.setState({
+          bookings: allBookings
+        }
+        );
+      })
+  }
+
+
+  render() {
+    //  let content = this.state.bookings.map((booking) => {
+    //   return (
+    //
+    //
+    //     <tr>
+    //     <td>
+    //    {
+    //     booking.booker.name
+    //     }
+    //     </td>
+    //     <td>
+    //     {
+    //       booking.time.slice(0, -3)
+    //     }
+    //     </td>
+    //
+    //     <td>
+    //      {
+    //       booking.partySize
+    //     }
+    //     </td>
+    //
+    //     <td>
+    //     {
+    //       booking.seatingTable.tableNumber
+    //     }
+    //     </td>
+    //     <td>
+    //     {
+    //       booking.date
+    //     }
+    //     </td>
+    //     <td>
+    //       {
+    //       booking.booker.phone
+    //     }
+    //     </td>
+    //
+    //      <button type ="button">edit</button>
+    //      <button type ="button">cancel </button> </tr>
+    //
+    //   );
+    // })
+
+    return <ReactTable data={this.state.bookings}
+      filterable
+      defaultFilterMethod={(filter, row) =>
+        String(row[filter.id]) === filter.value}
+      columns={[
+        {
+          Header: "Name",
+          accessor: "booker.name",
+          filterMethod: (filter, row) =>
+            row[filter.id].startsWith(filter.value) &&
+            row[filter.id].endsWith(filter.value)
+        },
+
+
+        {
+          Header: "Phone number",
+          id: "phone",
+          accessor: d => d.booker.phone,
+          filterMethod: (filter, rows) =>
+            matchSorter(rows, filter.value, { keys: ["booker.phone"] }),
+          filterAll: true
+        },
+
+        {
+          Header: "Number of Guests",
+          id: "partySize",
+          accessor: d => d.partySize,
+          filterMethod: (filter, rows) =>
+            matchSorter(rows, filter.value, { keys: ["partySize"] }),
+          filterAll: true
+        },
+
+        {
+          Header: "Table",
+          id: "seatingTable",
+          accessor: d => d.seatingTable.tableNumber,
+          filterMethod: (filter, rows) =>
+            matchSorter(rows, filter.value, { keys: ["seatingTable.tableNumber"] }),
+          filterAll: true
+        },
+
+
+        //onClick require the props function redirecting to edit and update booking
+
+
+        //onClick require the props function redirecting to delete  booking
+
+
+
+
+        {
+          Header: "Bookings",
+          accessor: "partySize",
+          id: "over",
+          Cell: ({ value }) => (value = 4 ? "Yes" : "No"),
+          filterMethod: (filter, row) => {
+            if (filter.value === "all") {
+              return true;
+            }
+            if (filter.value === "true") {
+              return row[filter.id] >= 4;
+            }
+            return row[filter.id] < 4;
+          },
+          Filter: ({ filter, onChange }) =>
+            <select
+              onChange={event => onChange(event.target.value)}
+              style={{ width: "100%" }}
+              value={filter ? filter.value : "all"}
+            >
+              <option value="all">Show All</option>
+              <option value="true">Large party</option>
+              <option value="false">Small party</option>
+            </select>
+        }
+      ]
+
       }
-
-
-    render() {
-     //  let content = this.state.bookings.map((booking) => {
-     //   return (
-     //
-     //
-     //     <tr>
-     //     <td>
-     //    {
-     //     booking.booker.name
-     //     }
-     //     </td>
-     //     <td>
-     //     {
-     //       booking.time.slice(0, -3)
-     //     }
-     //     </td>
-     //
-     //     <td>
-     //      {
-     //       booking.partySize
-     //     }
-     //     </td>
-     //
-     //     <td>
-     //     {
-     //       booking.seatingTable.tableNumber
-     //     }
-     //     </td>
-     //     <td>
-     //     {
-     //       booking.date
-     //     }
-     //     </td>
-     //     <td>
-     //       {
-     //       booking.booker.phone
-     //     }
-     //     </td>
-     //
-     //      <button type ="button">edit</button>
-     //      <button type ="button">cancel </button> </tr>
-     //
-     //   );
-     // })
-
-      return (
-      <div className="customer-page">
-        <h1 className="booking-h1">Customers</h1>
-        <ReactTable data={this.state.bookings}
-                          filterable
-                          defaultFilterMethod={(filter, row)=>
-                          String(row[filter.id])===filter.value}
-                          columns={[
-                            {
-                              Header: "Date",
-                              accessor: "date",
-                              filterMethod: (filter, row ) =>
-                              row[filter.id].startsWith(filter.value)&&
-                              row[filter.id].endsWith(filter.value)
-                            },
-
-
-                  {  Header: "Party Size",
-                    id: "partySize",
-                    accessor: d => d.partySize,
-                    filterMethod: (filter, rows) =>
-                      matchSorter(rows, filter.value, { keys: ["partySize"] }),
-                    filterAll: true
-                  },
-
-                {  Header: "Name",
-                  id: "booker.name",
-                  accessor: d => d.booker.name,
-                  filterMethod: (filter, rows) =>
-                    matchSorter(rows, filter.value, { keys: ["booker.name"] }),
-                  filterAll: true
-                },
-
-                { Header: "Contact",
-                id: "booker.phone",
-                accessor: d => d.booker.phone,
-                filterMethod: (filter, rows) =>
-                  matchSorter(rows, filter.value, { keys: ["booker.phone"] }),
-                filterAll: true
-              },
-
-
-                //onClick require the props function redirecting to edit and update booking
-
-
-                //onClick require the props function redirecting to delete  booking
+      defaultPageSize={10}
+      className="-striped -highlight"
 
 
 
+    />
+  }
 
-              {
-                    Header: "Bookings",
-                    accessor: "partySize",
-                    id: "over",
-                    Cell: ({ value }) => (value = 3 ? "Yes" : "No"),
-                    filterMethod: (filter, row) => {
-                      if (filter.value === "all") {
-                        return true;
-                      }
-                      if (filter.value === "true") {
-                        return row[filter.id] >= 3;
-                      }
-                      return row[filter.id] < 3;
-                    },
-                    Filter: ({ filter, onChange }) =>
-                      <select
-                        onChange={event => onChange(event.target.value)}
-                        style={{ width: "100%" }}
-                        value={filter ? filter.value : "all"}
-                      >
-                        <option value="all">Show All</option>
-                        <option value="true">Can Drink</option>
-                        <option value="false">Can't Drink</option>
-                      </select>
-                    }
-                  ]
+}
 
-<<<<<<< HEAD
-      }
-              defaultPageSize={10}
-              className="-striped -highlight"
-=======
-             {
-                   Header: "Bookings",
-                   accessor: "partySize",
-                   id: "over",
-                   Cell: ({ value }) => (value = 4 ? "Yes" : "No"),
-                   filterMethod: (filter, row) => {
-                     if (filter.value === "all") {
-                       return true;
-                     }
-                     if (filter.value === "true") {
-                       return row[filter.id] >= 4;
-                     }
-                     return row[filter.id] < 4;
-                   },
-                   Filter: ({ filter, onChange }) =>
-                     <select
-                       onChange={event => onChange(event.target.value)}
-                       style={{ width: "100%" }}
-                       value={filter ? filter.value : "all"}
-                     >
-                       <option value="all">Show All</option>
-                       <option value="true">Large party</option>
-                       <option value="false">Small party</option>
-                     </select>
-                  }
-                ]
-
-     }
-            defaultPageSize={10}
-            className="-striped -highlight"
-
->>>>>>> cf4df11e6168fb3a93501d24fb7c8eab95a5dc4f
-
-
-        
-          />
-        </div>
-        )} 
-    }
-  
-    export default AllCustomers;
+export default AllCustomers;
