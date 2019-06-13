cef0be8e951f113fbf9a56320e095861c1a79131
lidek <lidia.stopinska@gmail.com>
2019-06-13 17:11:32 +0100

    fixed edit and amend booking method

--- client/karl_bistro/src/components/manage_bookings/EditForm.js
+++ client/karl_bistro/src/components/manage_bookings/EditForm.js
@@ -20,7 +20,7 @@ class EditForm extends React.Component {
   }
 
   getBookingDetails() {
-    axios.get(`http://localhost:8080/bookings/allbookings/${this.props.id}`)
+    axios.get(`http://localhost:8080/bookings/${this.props.id}`)
       .then(response => {
         this.setState({
           id: response.data.id,
@@ -44,7 +44,7 @@ class EditForm extends React.Component {
     // .then(response => {
     //   this.setState({booker: booker})
     // })
-    axios.get(`http://localhost:8080/bookings/allbookings/${this.props.id}/${this.props.booker}`)
+    axios.get(`http://localhost:8080/bookings/${this.props.id}/${this.props.booker}`)
     .then(response => {
       this.setState({booking: booking})
     })
@@ -53,7 +53,7 @@ class EditForm extends React.Component {
       "date": this.state.date,
       "time": this.state.time,
       "partySize": this.state.partySize,
-      "booker": `http://localhost:8080/bookers/${this.props.id}`,
+      "booker": `http://localhost:8080/bookers/${this.state.booker.id}`,
       "seatingTable": this.state.table,
       "bookingNote": this.state.bookingNote
     };
